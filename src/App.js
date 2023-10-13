a6dbcd2f64bc07feaaa281777f34971fcfee592c
Hasan Sadhir Mohammed Mansoor <hsadhir@indeed.com>
2023-10-13 23:54:29 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+    return (
+            <h1>Welcome to React App that's built using Webpack and Babel separately!</h1>
+    );
+};
+
+export default App;
+
